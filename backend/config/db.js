abad136ecb05c938c60e14a36ee604e51ffe9290
S.Agalya <agalyaishere@gmail.com>
2025-07-07 20:40:34 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,24 @@
+// import pkg from "pg";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// pool.connect()
+//   .then(() => console.log("Connected to PostgreSQL"))
+//   .catch(err => console.error("Connection error", err.stack));
+
+// export default pool;
+
+
 import pkg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
@@ -5,11 +26,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()

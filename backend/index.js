2a0b6c7c293cec519ff5911970cf1fd85578f03f
S.Agalya <agalyaishere@gmail.com>
2025-09-20 11:29:47 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -25,5 +25,5 @@ app.get("/", (req, res) => {
 });
 app.use("/api/profile", profileRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

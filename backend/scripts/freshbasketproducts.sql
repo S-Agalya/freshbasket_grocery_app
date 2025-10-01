ac44a6ac56b72c650fb10e2fb123b4f8c43d4081
S.Agalya <agalyaishere@gmail.com>
2025-10-02 01:31:02 +0530

    added sql query scripts

---
+++ backend/scripts/freshbasketproducts.sql
@@ -0,0 +1,45 @@
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(1, 'Carrot', 30, 'carrot.jpg', 'Vegetables', 'kg'),
+(2, 'Cucumber', 25, 'cucumber.jpg', 'Vegetables', 'kg'),
+(3, 'Beetroot', 20, 'beetroot.jpg', 'Vegetables', 'kg'),
+(4, 'Tomato', 15, 'tomato.jpeg', 'Vegetables', 'kg'),
+(5, 'Capsicum', 35, 'capsicum.jpeg', 'Vegetables', 'kg');
+
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(101, 'Apple', 120, 'apple.jpg', 'Fruits', 'kg'),
+(102, 'Banana', 60, 'banana.jpg', 'Fruits', 'dozen'),
+(103, 'Orange', 80, 'orange.jpg', 'Fruits', 'kg'),
+(104, 'Papaya', 150, 'papaya.jpg', 'Fruits', 'piece'),
+(105, 'Grapes', 90, 'grapes.jpg', 'Fruits', 'kg');
+
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(201, 'Aavin Milk', 50, 'aavinmilk.jpg', 'Dairy', 'litre'),
+(202, 'Curd', 40, 'curd.jpg', 'Dairy', 'packet'),
+(203, 'Butter', 120, 'butter.jpg', 'Dairy', 'packet'),
+(204, 'Cheese', 150, 'cheese.jpg', 'Dairy', 'packet'),
+(205, 'Dodla Milk', 50, 'dodlamilk.jpg', 'Dairy', 'litre'),
+(206, 'Paneer', 90, 'paneer.jpg', 'Dairy', 'packet'),
+(207, 'Ghee Packet', 250, 'gheepacket.jpg', 'Dairy', 'packet'),
+(208, 'Ghee Bottle', 300, 'gheebottle.jpg', 'Dairy', 'bottle');
+
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(301, 'Lifebuoy Soap', 40, 'lifebuoy.jpg', 'Soaps', 'piece'),
+(302, 'Dove Soap', 60, 'dove.jpg', 'Soaps', 'piece'),
+(305, 'Surf Excel', 180, 'surfexcel.jpg', 'Detergents', 'packet'),
+(307, 'Rin', 90, 'rin.jpg', 'Detergents', 'packet'),
+(311, 'Sunsilk Shampoo', 130, 'sunsilk.jpg', 'Shampoos', 'bottle'),
+(312, 'Pantene Shampoo', 140, 'pantene.jpg', 'Shampoos', 'bottle'),
+(314, 'Dettol Handwash', 65, 'dettolhandwash.jpg', 'Handwash', 'bottle'),
+(315, 'Himalaya Handwash', 70, 'himalayahandwash.jpg', 'Handwash', 'bottle');
+
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(401, 'Chips', 30, 'chips.jpg', 'Snacks', 'packet'),
+(402, 'Biscuits', 25, 'biscuits.jpg', 'Snacks', 'packet'),
+(403, 'Chocolates', 80, 'chocolates.jpg', 'Snacks', 'packet'),
+(404, 'Namkeen', 60, 'namkeen.jpg', 'Snacks', 'packet');
+
+INSERT INTO products (id, name, price, image, category, unit_type) VALUES
+(501, 'Rice', 60, 'rice.jpg', 'Grocery', 'kg'),
+(502, 'Aashirvaad Atta', 55, 'wheat.jpg', 'Grocery', 'kg'),
+(503, 'Fortune Oil', 150, 'Cooking Oil.jpg', 'Grocery', 'litre'),
+(504, 'Sugar', 40, 'sugar.jpg', 'Grocery', 'kg');

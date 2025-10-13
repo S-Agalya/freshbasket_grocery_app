6dabbce8ec2e42cb865121ec45452f37b55126af
S.Agalya <agalyaishere@gmail.com>
2025-10-13 16:18:38 +0530

    commit

--- freshbasket-admin/vite.config.js
+++ freshbasket-admin/vite.config.js
@@ -8,26 +8,12 @@
 // })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//    build: {
-//     rollupOptions: {
-//       external: ['react/jsx-runtime']
-//     }
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   optimizeDeps: {
-    include: ["react/jsx-runtime", "react/jsx-dev-runtime"]
-  }
+  plugins: [react({
+    jsxRuntime:'classic'
+  })],
 })

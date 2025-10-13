5e2fd254a2d1a7629ba373e09cb0100fd88bc68a
S.Agalya <agalyaishere@gmail.com>
2025-10-14 01:34:05 +0530

    Fix CORS for frontend on Render

--- backend/index.js
+++ backend/index.js
@@ -36,3 +36,49 @@
 // //app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // const PORT = process.env.PORT ;
 // app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { register, login } from "./controllers/authController.js";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
+import path from "path";
+import adminProductRoutes from "./routes/adminProductRoutes.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    "https://freshbasket-grocery-app-admin.onrender.com",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+// ✅ your routes
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/admin/products", adminProductRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.get("/", (req, res) => {
+  res.send("FreshBasket backend is running 🚀");
+});
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT ;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

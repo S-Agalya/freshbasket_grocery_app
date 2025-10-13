47342e1ed894310c4efcf281fa0b43a91846bf48
S.Agalya <agalyaishere@gmail.com>
2025-10-13 16:22:42 +0530

    commit

--- freshbasket-admin/vite.config.js
+++ freshbasket-admin/vite.config.js
@@ -8,12 +8,29 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react({
+//     jsxRuntime:'classic'
+//   })],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime:'classic'
-  })],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js'
+    }
+  },
+  build: {
+    outDir: "dist"
+  },
 })

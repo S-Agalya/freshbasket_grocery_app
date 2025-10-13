120635e2b752bd5168285b07a1436569c8340b75
S.Agalya <agalyaishere@gmail.com>
2025-10-13 14:55:32 +0530

    commit

--- freshbasket-admin/vite.config.js
+++ freshbasket-admin/vite.config.js
@@ -14,4 +14,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['react/jsx-runtime']
+    }
+  },
 })

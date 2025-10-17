442579531a23628b8b1e063d6551198543a9748d
S.Agalya <agalyaishere@gmail.com>
2025-10-18 01:21:40 +0530

    fixed add product route

--- backend/routes/adminProductRoutes.js
+++ backend/routes/adminProductRoutes.js
@@ -10,7 +10,7 @@ import { upload } from "../middleware/upload.js";
 const router = express.Router();
 
 // Add product (with image)
-router.post("/", upload.single("image"), addAdminProduct);
+router.post("/add", upload.single("image"), addAdminProduct);
 
 // Update product (image optional)
 router.put("/:id", upload.single("image"), updateAdminProduct);

a3f6efc87ecc235be8fa68db53f7f0e4b0dc615e
Abinaisree <141222753+Abinaisree@users.noreply.github.com>
2026-07-11 20:07:21 +0530

    updated the gemimi model

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -624,7 +624,7 @@ Return ONLY JSON.
 `;
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-2.0-flash",
       contents: prompt,
     });
 
